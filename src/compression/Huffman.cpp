a490826526ffe2a71b936e2e891a82a43c4278ec
U-IN\korotetskyi <korotetskyi@PC2014017.in.elcom.sk>
2021-08-25 13:25:16 +0200

    Intercommit 3

--- src/compression/Huffman.cpp
+++ src/compression/Huffman.cpp
@@ -13,61 +13,69 @@ void Huffman::Decompress(void* stream) {
 
 }
 
-void Huffman::SortAndMerge(std::list<Node*> nodes){
+void Huffman::SortAndMerge(std::list<HuffmanNode*> tree){
 
-    while (nodes.size() != 1) {
+    while (tree.size() != 1) {
         
-        nodes.sort(CompareObj());
+        tree.sort(HuffmanNode::Compare());    // compare func or compare obj?
 
-        Node *L = nodes.front();
-        nodes.pop_front();
-        Node *R = nodes.front();
-        nodes.pop_front();
+        HuffmanNode *L = tree.front();
+        tree.pop_front();
+        HuffmanNode *R = tree.front();
+        tree.pop_front();
 
-        Node *P = (Node*) calloc(1, sizeof(Node));
-        P->left = L;
-        P->right = R;
-        P->value = L->value + R->value;
-
-        nodes.push_back(P);
+        HuffmanNode *P = new HuffmanNode(L, R);
+        tree.push_back(P);
 
     }
-    
 }
 
-void Huffman::AchieveNodes(std::map<uint8_t, uint32_t> distribution){
+void Huffman::CreateCodeTable(HuffmanNode *root) {
+    if(root->left != nullptr) {
+        this->code.push_back(0);
+        CreateCodeTable(root->left);
+    }
 
-    for(auto i = distribution.begin(); i != distribution.end(); i++) {
-        Node *n = (Node *) calloc(1, sizeof(Node));
-        n->key = i->first;
-        n->value = i->second;
+    if(root->right != nullptr) {
+        this->code.push_back(1);
+        CreateCodeTable(root->right);
+    }
 
-        n->left = NULL;
-        n->right = NULL;
+    if(root->left == nullptr && root->right == nullptr) {
+        this->table[root->key] = this->code;
+    }
 
-        this->nodes.push_back(n);
+    this->code.pop_back();
+}
+
+void Huffman::AchieveNodes(void){
+
+    for(auto i = this->distribution.begin(); i != this->distribution.end(); i++) {
+        HuffmanNode *n = new HuffmanNode(i->first, i->second);
+        this->tree.push_back(n);
     }
 }
 
 void Huffman::FreeNodes(Node *node) {
-    if(node == NULL) {
+    if(node == nullptr) {
         return;
     }
 
     FreeNodes(node->left);
     FreeNodes(node->right);
 
-    free(node);
+    delete node;
 }
 
 uint8_t Huffman::ComputeDistribution(int32_t fd) {
 
     ssize_t ret = 0;
     uint8_t buf = 0;
-    uint8_t fileOffset1 = 0;
+    uint64_t fileOffset1 = 0;
 
     uint64_t mapCounter = 0;
-    
+
+    // slow positional read
     while((ret = pread(fd, &buf, 1, fileOffset1)) != 0){
         if(ret == -1){
             if (errno == EINTR) continue; // handling some frequent interruptions
@@ -100,32 +108,3 @@ bool Huffman::CheckMap(std::map<uint8_t, uint32_t> m, uint8_t key) {
 
     return (m.find(key) == m.end()) ? false : true;
 }
-
-    // for(uint32_t i = 0; i < sb.st_size+1; i++) {
-    //      printf("%u:\t" PRINTF_BINARY_PATTERN_INT16 "\n", i, PRINTF_BYTE_TO_BINARY_INT16((int)this->fstream[i]));
-    //}
-
-
-/*
-size_t slow_copy(int fd1, int fd2, off_t fileOffset1, off_t fileOffset2, int amount){
-    char buf;
-    ssize_t ret;
-    
-    while((ret = pread(fd1, &buf, 1, fileOffset1)) != 0 && (amount == WHOLEFILE || amount > 0)){
-        if(ret == -1){
-            if (errno == EINTR) continue; // handling some frequent interruptions
-            return E_READ;
-        }
-        fileOffset1++;
-        if((ret = pwrite(fd2, &buf, 1, fileOffset2)) != 0){
-            if(ret == -1){
-                if (errno == EINTR) continue; // handling some frequent interruptions
-                return E_WRITE;
-            }
-        }
-        if(amount != WHOLEFILE) amount -= (int) ret;
-        fileOffset2++;
-    }
-    return SUCCESS;
-}
-*/
--- src/compression/Huffman.h
+++ src/compression/Huffman.h
@@ -12,57 +12,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+
+#include "HuffmanNode.h"
 #include "../../conf/conf.h"
 #include "../datatypes.h"
 #include "../../conf/conf.h"
 #include "Compressor.h"
 
-namespace huffman {
+class Huffman : public Compressor {
 
-    typedef struct Nodes {
-        uint8_t key;
-        uint32_t value;
-        struct Nodes* left;
-        struct Nodes* right;
-    } Node;
+    private:
+        std::map<uint8_t, uint32_t> distribution = {};
+        std::list<HuffmanNode*> tree = {};
+        std::vector<bool> code = {};
+        std::map<uint8_t, std::vector<bool>> table = {};
+        int32_t fd;
+        
+    public:
 
-    struct CompareObj {
-        bool operator()(Node* l, Node* r) const {
-            return l->value != r->value ? l->value < r->value : l->key < r->key;
+        Huffman(int32_t fd) {
+            this->fd = fd;
+        };
+
+        void Compress(void* stream);
+        void Decompress(void* stream);
+
+        ~Huffman() {
+            FreeNodes(this->tree.front());
         }
-    };
- 
-    class Huffman : public Compressor {
-        private:
-            std::map<uint8_t, uint32_t> distribution = {};
-            std::list<Node*> nodes = {};
-            std::vector<bool> code = {};
-            std::map<uint8_t, std::vector<bool>> table = {};
-            int32_t fd;
         
+    private:
+        uint8_t ComputeDistribution(int32_t fd);
+        void AchieveNodes(void);
+        bool CheckMap(std::map<uint8_t, uint32_t> m, uint8_t key);
+        void SortAndMerge(std::list<HuffmanNode*> tree);
+        void FreeNodes(HuffmanNode *node);
+        void CreateCodeTable(HuffmanNode *root);
         
-        public:
-
-            Huffman(int32_t fd) {
-                this->fd = fd;
-            };
-
-            void Compress(void* stream);
-            void Decompress(void* stream);
-
-            ~Huffman() {
-                FreeNodes(nodes.front());
-            }
-        
-        private:
-            uint8_t ComputeDistribution(int32_t fd);
-            void AchieveNodes(std::map<uint8_t, uint32_t> distribution);
-            bool CheckMap(std::map<uint8_t, uint32_t> m, uint8_t key);
-            void SortAndMerge(std::list<Node*> nodes);
-            void FreeNodes(Node *node);
-        
-    };
-
 };
+
 
 #endif
